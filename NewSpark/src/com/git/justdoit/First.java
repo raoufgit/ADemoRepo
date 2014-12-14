cf3ea2ce521312c1c5ecacd353de696b8aa28e91
Mohammed Abdul Raouf <mohra10@ca.com>
2014-12-14 03:35:06 -0500

    Great Work Done

--- NewSpark/src/com/git/justdoit/First.java
+++ NewSpark/src/com/git/justdoit/First.java
@@ -6,7 +6,7 @@ public class First {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Great Work done");
 
 	}
 
--- NewSpark/src/com/git/justdoit/Light.java
+++ NewSpark/src/com/git/justdoit/Light.java
@@ -6,7 +6,8 @@ public class Light {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Great Work done");
 
 	}
 
